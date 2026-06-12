25aea09bbedd0a203782fb4c999458206c01acbe
zxfancy <zxfancy@gmail.com>
2026-06-12 11:47:30 +0800

    v2：新增11点前睡觉打卡、社交活动计数、健身课运动类型；自摸改名小快乐

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "lifelog-v1";
+const CACHE = "lifelog-v2";
 const SHELL = ["./", "index.html", "manifest.json", "icon-192.png", "icon-512.png"];
 
 self.addEventListener("install", e => {
