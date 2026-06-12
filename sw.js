81488904f4ded936bf1fc6c6887c010cee2aab09
zxfancy <zxfancy@gmail.com>
2026-06-12 11:51:37 +0800

    v3：日历格子改用表情图标标记，新增选中日详情面板，图例简化

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "lifelog-v2";
+const CACHE = "lifelog-v3";
 const SHELL = ["./", "index.html", "manifest.json", "icon-192.png", "icon-512.png"];
 
 self.addEventListener("install", e => {
